b252b4c20fe198c2e817aef6daef4cb86c187733
Christian Krause <christian.krause@idiv.de>
2016-12-26 20:38:42 +0100

    allow to choose different hashes

--- archive-sum.c
+++ archive-sum.c
@@ -3,13 +3,14 @@
 #include <archive.h>
 #include <archive_entry.h>
 
-#include <openssl/md5.h>
+#include <openssl/evp.h>
 
-int archive_sum(const char *filename) {
+int archive_sum(const EVP_MD *md, const char *filename) {
   struct archive *a;
   struct archive_entry *e;
 
-  int n, r;
+  int r;
+  unsigned int i, md_len;
 
   struct stat s;
 
@@ -33,31 +34,30 @@ int archive_sum(const char *filename) {
     return EXIT_FAILURE;
   }
 
+  EVP_MD_CTX *mdctx = EVP_MD_CTX_create();
+  unsigned char md_value[EVP_MAX_MD_SIZE];
+
   while (archive_read_next_header(a, &e) == ARCHIVE_OK) {
     if (archive_entry_filetype(e) != AE_IFREG)
       continue;
 
     la_ssize_t size;
     char buf[bsize];
-    MD5_CTX c;
-    unsigned char md[16];
 
-    MD5_Init(&c);
+    EVP_DigestInit_ex(mdctx, md, NULL);
 
     while ((size = archive_read_data(a, &buf, bsize)) > 0)
-      MD5_Update(&c, buf, size);
+      EVP_DigestUpdate(mdctx, buf, size);
 
-    MD5_Final(md, &c);
+    EVP_DigestFinal_ex(mdctx, md_value, &md_len);
 
-    char *out = (char*)malloc(33);
+    for (i = 0; i < md_len; i++)
+      printf("%02x", md_value[i]);
 
-    for (n = 0; n < 16; n++)
-      snprintf(&(out[n*2]), 16*2, "%02x", (unsigned int)md[n]);
-
-    printf("%s  %s\n", out, archive_entry_pathname(e));
-
-    free(out);
+    printf("  %s\n", archive_entry_pathname(e));
   }
+
+  EVP_MD_CTX_destroy(mdctx);
 
   r = archive_read_free(a);
 
@@ -69,11 +69,28 @@ int archive_sum(const char *filename) {
   return EXIT_SUCCESS;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   int i;
 
-  for (i = 1; i < argc; i++)
-    archive_sum(argv[i]);
+  if (argc < 3) {
+    printf("usage: %s hash file..\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  OpenSSL_add_all_digests();
+
+  const EVP_MD *md = EVP_get_digestbyname(argv[1]);
+
+  if (!md) {
+    fprintf(stderr, "unknown digest: %s\n", argv[1]);
+    EVP_cleanup();
+    return EXIT_FAILURE;
+  }
+
+  for (i = 2; i < argc; i++)
+    archive_sum(md, argv[i]);
+
+  EVP_cleanup();
 
   return EXIT_SUCCESS;
 }
